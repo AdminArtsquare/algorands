679dc349e448fa3c63495bd0680eeee43479a0fd
Marco Sanna <marco.sanna@talete.net>
2021-10-07 18:02:13 +0200

    deleted files

--- base.js
+++
@@ -1,9 +0,0 @@
-// method for printing logs in textarea and console
-function addLog(text, new_line = true){
-    text = JSON.stringify(text);
-    if(new_line)
-        $("#textarea").append(text + "\n");
-    else
-        $("#textarea").append(text);
-    console.log(text);
-}
